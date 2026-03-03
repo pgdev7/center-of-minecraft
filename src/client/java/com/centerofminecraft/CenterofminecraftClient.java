d3ed2ad81cabc48384dc49dc07867638976d2948
Pranav <pgdev7@gmail.com>
2026-03-03 17:18:02 -0500

    startup code

--- src/client/java/com/centerofminecraft/CenterofminecraftClient.java
+++ src/client/java/com/centerofminecraft/CenterofminecraftClient.java
@@ -1,10 +1,12 @@
 package com.centerofminecraft;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 
 public class CenterofminecraftClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		MobSkinManager.register(EntityType.CREEPER, ResourceLocation.fromNamespaceAndPath("centerofminecraft", "textures/entity/creeper.png"));
 	}
 }
